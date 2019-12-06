6ed771d597196ce42e819264aedcd8c8dd1c781e
Eduardo C. Vilhalba <vilhalba.20000@gmail.com>
2019-12-06 16:21:30 -0300

    Update Viagem.java

--- pootrab/src/main/java/com/pootrab/models/Viagem.java
+++ pootrab/src/main/java/com/pootrab/models/Viagem.java
@@ -5,93 +5,22 @@ import java.util.List;
 
 
 public class Viagem {
+	
+	
+	/*
+	 * NO MINIMO UMA PESSOA COM TIPO MOTORISTA
+	 * MANY TO MANY EM PESSOA
+	 * UNIDADE ONE TO MANY
+	 * TIPO DESCOLAMENTO ONE TO MANY
+	 * VEICULO ONE TO MANY
+	 * SITUAÇAO ONE TO MANY
+	 * 
+	 */
 	private float vlCustoEstMotorista;
 	private float vlCustoEstVeiculo;
-	private int qtdPassageiros;
-	private Unidade unidade;
-	private TipoDeslocamento tipoDeslocamento;
-	//private DataSolicitacao dtSolicitacao;
-	private Veiculo veiculo;
-	private Situacao situacao;
-	private List<Passageiro> passageiros;
-	private Motorista motorista;
+	private String dtSolicitacao;
+
+
 	
-	public Motorista getMotorista() {
-		return motorista;
-	}
-
-	public void setMotorista(Motorista motorista) {
-		this.motorista = motorista;
-	}
-
-	public List<Passageiro> getPassageiros() {
-		return passageiros;
-	}
-
-	public void setPassageiros(List<Passageiro> passageiros) {
-		this.passageiros = passageiros;
-	}
-
-	public Viagem(){}
 	
-	public Viagem(float vlCustoEstMotorista, float vlCustoEstVeiculo, int qtdPassageiros, Unidade unidade, TipoDeslocamento tipoDeslocamento,/* DataSolicitacao dtSolicitacao */, Veiculo veiculo, Situacao situacao ) {
-		super();
-		this.vlCustoEstMotorista=vlCustoEstMotorista;		
-		this.vlCustoEstVeiculo=vlCustoEstVeiculo;
-		this.qtdPassageiros=qtdPassageiros;
-		this.unidade=unidade;
-		this.tipoDeslocamento=tipoDeslocamento;
-		//this.dtSolicitacao=dtSolicitacao;
-		this.veiculo=veiculo;
-		this.situacao=situacao;
-	}
-	
-	public float getVlCustoEstMotorista() {
-		return vlCustoEstMotorista;
-	}
-	public void setVlCustoEstMotorista(float vlCustoEstMotorista) {
-		this.vlCustoEstMotorista = vlCustoEstMotorista;
-	}
-	public float getVlCustoEstVeiculo() {
-		return vlCustoEstVeiculo;
-	}
-	public void setVlCustoEstVeiculo(float vlCustoEstVeiculo) {
-		this.vlCustoEstVeiculo = vlCustoEstVeiculo;
-	}
-	public int getQtdPassageiros() {
-		return qtdPassageiros;
-	}
-	public void setQtdPassageiros(int qtdPassageiros) {
-		this.qtdPassageiros = qtdPassageiros;
-	}
-	public Unidade getUnidade() {
-		return unidade;
-	}
-	public void setUnidade(Unidade unidade) {
-		this.unidade = unidade;
-	}
-	public TipoDeslocamento getTipoDeslocamento() {
-		return tipoDeslocamento;
-	}
-	public void setTipoDeslocamento(TipoDeslocamento tipoDeslocamento) {
-		this.tipoDeslocamento = tipoDeslocamento;
-	}
-	public DataSolicitacao getDtSolicitacao() {
-		return dtSolicitacao;
-	}
-	public void setDtSolicitacao(DataSolicitacao dtSolicitacao) {
-		this.dtSolicitacao = dtSolicitacao;
-	}
-	public Veiculo getVeiculo() {
-		return veiculo;
-	}
-	public void setVeiculo(Veiculo veiculo) {
-		this.veiculo = veiculo;
-	}
-	public Situacao getSituacao() {
-		return situacao;
-	}
-	public void setSituacao(Situacao situacao) {
-		this.situacao = situacao;
-	}
 }
